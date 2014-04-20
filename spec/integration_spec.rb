ad1bce7057cef12a3390dd1f660f20f6782a73af
Xavier Shay <xavier@rhnh.net>
2014-04-20 10:24:31 -0700

    Test coverage for response source.

---
+++ spec/integration_spec.rb
@@ -0,0 +1,182 @@
+require 'integration_helper'
+
+describe 'Poniard controller', type: :controller do
+  context '#default' do
+    render_views
+
+    poniard_controller do
+      def index(response)
+        response.default message: "implicit"
+      end
+    end
+
+    it 'renders template matching method name with instance variables' do
+      get :index
+      expect(response.body).to match(/Message: implicit/)
+    end
+  end
+
+  context '#render' do
+    render_views
+
+    poniard_controller do
+      def index(response)
+        response.render action: 'index',
+          headers: {'Content-Type' => 'text/plain'},
+          ivars: {message: 'explicit'}
+      end
+    end
+
+    it 'renders explicit template with instance variables' do
+      get :index
+      expect(response.body).to match(/Message: explicit/)
+    end
+
+    it 'can set headers' do
+      get :index
+      expect(response.headers['Content-Type']).to eq('text/plain')
+    end
+  end
+
+  context '#render_action' do
+    render_views
+
+    poniard_controller do
+      def index(response)
+        response.render_action 'index', message: 'explicit'
+      end
+    end
+
+    it 'renders explicit template with instance variables' do
+      get :index
+      expect(response.body).to match(/Message: explicit/)
+    end
+  end
+
+  describe '#head' do
+    poniard_controller do
+      def index(response)
+        response.head 422
+      end
+    end
+
+    it 'sets response code' do
+      get :index
+      expect(response.status).to eq(422)
+    end
+  end
+
+  describe '#redirect_to' do
+    # See fake admin_* route methods in integration_helper.rb
+
+    context 'path' do
+      poniard_controller do
+        def index(response)
+          response.redirect_to :admin, :secret
+        end
+      end
+
+      it 'redirects' do
+        get :index
+        expect(response).to redirect_to("/admin/secret")
+      end
+    end
+
+    context 'url' do
+      poniard_controller do
+        def index(response)
+          response.redirect_to :admin_url, :secret
+        end
+      end
+
+      it 'redirects' do
+        get :index
+        expect(response).to redirect_to("http://foo/admin/secret")
+      end
+    end
+
+    context 'aribtrary' do
+      poniard_controller do
+        def index(response)
+          response.redirect_to '/some_path'
+        end
+      end
+
+      it 'redirects' do
+        pending "Unimplemented"
+        get :index
+        expect(response).to redirect_to("/some_path")
+      end
+    end
+  end
+
+  context '#redirect_to_action' do
+    poniard_controller do
+      def index(response)
+        response.redirect_to_action :new
+      end
+
+      def new
+      end
+    end
+
+    it 'redirects' do
+      pending "Not sure correct rspec-rails scaffolding to make this work"
+      get :index
+      expect(response).to redirect_to(:new)
+    end
+  end
+
+  describe '#send_data' do
+    poniard_controller do
+      def index(response)
+        response.send_data "data", type: "text/plain"
+      end
+    end
+
+    it 'sends body' do
+      get :index
+      expect(response.body).to eq("data")
+    end
+
+    it 'sets headers' do
+      get :index
+      expect(response.headers['Content-Type']).to eq("text/plain")
+    end
+  end
+
+  describe '#respond_with' do
+    render_views
+
+    poniard_controller do
+      def index(response)
+        responder = Class.new do
+          def initialize(body)
+            @body = body
+          end
+
+          def html(response); response.render text: "<b>#{@body}</b>" end
+          def text(response); response.render text: @body end
+        end
+
+        response.respond_with responder, "body"
+      end
+    end
+
+    it 'responds with html' do
+      get :index
+      expect(response.body).to eq("<b>body</b>")
+    end
+
+    it 'responds with text' do
+      get :index, format: :text
+      expect(response.body).to eq("body")
+    end
+
+    it 'handles unknown format' do
+      pending "Unimplemented"
+      get :index, format: :json
+      expect(response.status).to eq(422)
+    end
+  end
+end
